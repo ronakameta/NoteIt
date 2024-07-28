23380a578c91516f273c50b02275fd836ca480f1
Ronak <ametaronak@gmail.com>
2024-07-28 14:44:07 +0530

    updates

--- script.js
+++ script.js
@@ -6,63 +6,63 @@ let addNoteBtn = document.querySelector(".icon-add");
 let todoContainer = document.querySelector(".todo-container");
 
 //array of notes object
-let arrayofnotes = JSON.parse(localStorage.getItem("notes")) || []
-
-
+let arrayofnotes = JSON.parse(localStorage.getItem("notes")) || [];
 
 //adding the note
-addNoteBtn.addEventListener("click", () =>{
-    if(note.value.trim().length > 0 || title.value.trim().length > 0){
-        arrayofnotes =  [...arrayofnotes, {id: Date.now(), title: title.value
-        ,note: note.value, isarchive: false 
-    }]
-    }
-    console.log(arrayofnotes);
-    localStorage.setItem("notes", JSON.stringify(arrayofnotes));
-    todoContainer.innerHTML = 
-    renderNotes(arrayofnotes.filter(element => element.isarchive == false))
-
-
-    title.value = note.value = "";
-    
-})
-
-todoContainer.addEventListener(("click"), (e) =>{
-    // console.log(e.target.dataset.id, e.target.dataset.type);
-    let type = e.target.dataset.type;
-    let noteId = e.target.dataset.id;
-    if(type == "del"){
-  arrayofnotes = 
-  arrayofnotes.filter(element => element.id.toString() !== noteId)
-  todoContainer.innerHTML = renderNotes(arrayofnotes.filter(element => element.isarchive == false)); 
+addNoteBtn.addEventListener("click", () => {
+  if (note.value.trim().length > 0 || title.value.trim().length > 0) {
+    arrayofnotes = [
+      ...arrayofnotes,
+      {
+        id: Date.now(),
+        title: title.value,
+        note: note.value,
+        isarchive: false,
+      },
+    ];
+  }
+  console.log(arrayofnotes);
   localStorage.setItem("notes", JSON.stringify(arrayofnotes));
-}
-if(type == "arch"){
-    arrayofnotes = arrayofnotes.map((element) => 
-    { return element.id.toString()===noteId ? {...element, isarchive:
-         !element.isarchive} : element })
-        console.log(arrayofnotes);
-    todoContainer.innerHTML = 
-    renderNotes(arrayofnotes.filter(element => element.isarchive == false))    
-    localStorage.setItem("notes", JSON.stringify(arrayofnotes));
+  todoContainer.innerHTML = renderNotes(
+    arrayofnotes.filter((element) => element.isarchive == false)
+  );
 
-}
-})
+  title.value = note.value = "";
+});
+
+todoContainer.addEventListener("click", (e) => {
+  // console.log(e.target.dataset.id, e.target.dataset.type);
+  let type = e.target.dataset.type;
+  let noteId = e.target.dataset.id;
+  if (type == "del") {
+    arrayofnotes = arrayofnotes.filter(
+      (element) => element.id.toString() !== noteId
+    );
+    todoContainer.innerHTML = renderNotes(
+      arrayofnotes.filter((element) => element.isarchive == false)
+    );
+    localStorage.setItem("notes", JSON.stringify(arrayofnotes));
+  }
+  if (type == "arch") {
+    arrayofnotes = arrayofnotes.map((element) => {
+      return element.id.toString() === noteId
+        ? { ...element, isarchive: !element.isarchive }
+        : element;
+    });
+    console.log(arrayofnotes);
+    todoContainer.innerHTML = renderNotes(
+      arrayofnotes.filter((element) => element.isarchive == false)
+    );
+    localStorage.setItem("notes", JSON.stringify(arrayofnotes));
+  }
+});
 
 // todoContainer.innerHTML = renderNotes(arrayofnotes);
-todoContainer.innerHTML = 
-renderNotes(arrayofnotes.filter(element => element.isarchive == false))
+todoContainer.innerHTML = renderNotes(
+  arrayofnotes.filter((element) => element.isarchive == false)
+);
 
-
-
-
-
-
-
-
-
-
-//creating card 
+//creating card
 // function createCard(todo){
 //   const card = document.createElement("div");
 //   card.classList = "card";
